8e82a8d24473010adfaa82cbaa4cca6f9571108e
Chiahao <chiahao.codes@gmail.com>
2022-10-07 01:11:58 -0700

    commit main

--- script.js
+++ script.js
@@ -147,6 +147,7 @@ numberButtons.forEach((button) => {
 });
 
 
+
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (!currentOperandTextElement.innerText) {
